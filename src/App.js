b0945251b6824bc60d5a759f7457b1b3e3c0c6be
An Huynh <anthuynh81@gmail.com>
2020-05-31 22:23:10 -0400

    Changes to Gui

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import "./App.css";
 export const App = () => {
   const [fish, setFish] = useLocalStorage("fish", 0);
   const [yarn, setYarn] = useLocalStorage("yarn", 0);
+  var time = new Date().getHours() + ':' + new Date().getMinutes();
 
   return (
     <Container>
@@ -18,16 +19,38 @@ export const App = () => {
         <Col>
           <Header />
         </Col>
+        
       </Row>
       <Row>
-        <h3>Resources:</h3>
+        <Col >
+          <h3>Logs:</h3>
+        </Col>
+      </Row>
+      <Row md={4} className="textLog" >
+          <Col>
+              [{time}]: 
+          </Col>
+      </Row>
+
+      <Row>
+        <Col md={7}>
+          <h3>Resources:</h3>
+        </Col>
+        <Col>
+          <h3>Cats:</h3>
+        </Col>
       </Row>
 
       <Row>
         <Col md={5} className="secResources">
           <Resources fish={fish} setFish={setFish} yarn={yarn} setYarn={setYarn} />
         </Col>
-        <Col md={7}>PLACEHOLDER</Col>
+
+        <Col md={2}></Col>
+
+        <Col md={5} className="secCats">
+          PLACEHOLDER
+        </Col>
       </Row>
     </Container>
   );
